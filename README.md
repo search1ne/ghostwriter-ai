9b356d0debdd52686985652724da1187179f652d
search1ne <114374119+search1ne@users.noreply.github.com>
2023-01-02 19:51:52 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # buildspace 
 ### Welcome 👋
 This is the starter template for the [build your own AI writing assistant w/ GPT-3](https://buildspace.so/builds/ai-writer) project.
+
+
+# cd into the repo
+cd gpt3-writer-starter
+
+# install next if you don't have it
+yarn add next react react-dom
+
+# run it
+yarn dev
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import luckyzero from '../assets/lucky2647.jpg'
 import { useState } from 'react';
 
+
 const Home = () => {
-  const [ userInput, setUserInput ] = useState('');
-  const [apiOutput, setApiOutput] = useState('')
-  const [isGenerating, setIsGenerating] = useState(false)
-  
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+
   const callGenerateEndpoint = async () => {
     setIsGenerating(true);
-    
-    console.log("Calling OpenAI...")
+
+    console.log('Calling OpenAI...');
     const response = await fetch('/api/generate', {
       method: 'POST',
       headers: {
@@ -19,16 +21,15 @@ const Home = () => {
       },
       body: JSON.stringify({ userInput }),
     });
-  
+
     const data = await response.json();
     const { output } = data;
-    console.log("OpenAI replied...", output.text)
-  
+    console.log('OpenAI replied...', output.text);
+
     setApiOutput(`${output.text}`);
     setIsGenerating(false);
-  }
+  };
 
-  // 2. create a function that handles the click event
   const onUserChangedText = (event) => {
     setUserInput(event.target.value);
   };
@@ -44,47 +45,37 @@ const Home = () => {
             <h2>discover ancient knowledge through rhymes</h2>
           </div>
         </div>
-        {/* Add this code here*/}
+        
         <div className="prompt-container">
-          <textarea 
-            placeholder="ask the great stoics and philosophers: on love, on patience, on anger, on suffering..." 
-            className="prompt-box" 
-            value={ userInput }
-            onChange={ onUserChangedText }
-            />
-            
+          <textarea
+            placeholder="ask the great stoics and philosophers: on love, on patience, on anger, on suffering..."
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
         </div>
-        {/* Add this code here*/}
         <div className="prompt-buttons">
-          <a className={isGenerating ? 'generate-button loading' : 'generate-button'}
-    onClick={callGenerateEndpoint}>
+          <button className={isGenerating ? 'generate-button loading' : 'generate-button'} onClick={callGenerateEndpoint}>
             <div className="generate">
-            {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
-    </div>
-          </a>
+              {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+            </div>
+          </button>
         </div>
-
-        {/* New code I added here */}
         {apiOutput && (
-        <div className="output">
-          <div className="output-header-container">
-            <div className="output-header">
-              <h3>Output</h3>
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
             </div>
           </div>
-          <div className="output-content">
-            <p>{apiOutput}</p>
-          </div>
-        </div>
-      )}
+        )}
       </div>
-      
       <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://buildspace.so/builds/ai-writer" target="_blank" rel="noreferrer">
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
             <p>build with buildspace</p>
